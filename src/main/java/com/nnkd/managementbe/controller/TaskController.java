001ed1026ef44924872f7995104424db3e3263ed
NguyenNgocKhanhDuy <21130035@st.hcmuaf.edu.vn>
2025-01-18 20:36:27 +0700

    test(Task): comment schedule task

--- src/main/java/com/nnkd/managementbe/controller/TaskController.java
+++ src/main/java/com/nnkd/managementbe/controller/TaskController.java
@@ -293,47 +293,47 @@ public class TaskController {
         }
     }
 
-    @Scheduled(fixedRate = 60000)
-    public void checkTaskDeadline() {
-        List<TaskResponse> tasks = taskResponseService.getAlls();
-
-        Instant nowUtc = Instant.now();
-        LocalDateTime nowLocal = LocalDateTime.ofInstant(nowUtc, ZoneId.of("UTC+7"));
-
-
-        for (TaskResponse task : tasks) {
-            if (!task.isSend() && task.getDeadline() != null) {
-                System.out.println(task);
-                Instant deadline = task.getDeadline();
-
-                if (deadline.minus(Duration.ofHours(12)).isBefore(nowLocal.toInstant(ZoneOffset.UTC)) && !task.isSend()) {
-                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM dd, yyyy, HH:mm")
-                            .withZone(ZoneId.of("UTC"));
-
-                    ProjectResponse project = projectResponseService.getProjectById(task.getProject());
-                    User user = userService.getUserById(task.getCreator());
-
-                    String subject = String.format("Reminder: Task \"%s\" Deadline Approaching", task.getName());
-
-
-                    String text = String.format("Dear %s,\n\nThis is a reminder that the deadline for your task \"%s\" is approaching in less than 12 hours.\n\n" +
-                                    "**Task in Project:** %s\n" +
-                                    "- **Task Name:** %s\n" +
-                                    "- **Deadline:** %s\n\n" +
-                                    "Please ensure that you complete the task before the deadline.\n\n" +
-                                    "Best regards,\nNNKD",
-                            user.getUsername(), task.getName(), project.getName(), task.getName(), formatter.format(deadline));
-
-                    MailSendRequest mailSendRequest = MailSendRequest
-                            .builder()
-                            .to(user.getEmail()).text(text).subject(subject).build();
-
-                    mailService.sendMail(mailSendRequest);
-                    taskRequestService.updateSendMailDeadline(task.getId());
-                }
-            }
-        }
-    }
+//    @Scheduled(fixedRate = 60000)
+//    public void checkTaskDeadline() {
+//        List<TaskResponse> tasks = taskResponseService.getAlls();
+//
+//        Instant nowUtc = Instant.now();
+//        LocalDateTime nowLocal = LocalDateTime.ofInstant(nowUtc, ZoneId.of("UTC+7"));
+//
+//
+//        for (TaskResponse task : tasks) {
+//            if (!task.isSend() && task.getDeadline() != null) {
+//                System.out.println(task);
+//                Instant deadline = task.getDeadline();
+//
+//                if (deadline.minus(Duration.ofHours(12)).isBefore(nowLocal.toInstant(ZoneOffset.UTC)) && !task.isSend()) {
+//                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM dd, yyyy, HH:mm")
+//                            .withZone(ZoneId.of("UTC"));
+//
+//                    ProjectResponse project = projectResponseService.getProjectById(task.getProject());
+//                    User user = userService.getUserById(task.getCreator());
+//
+//                    String subject = String.format("Reminder: Task \"%s\" Deadline Approaching", task.getName());
+//
+//
+//                    String text = String.format("Dear %s,\n\nThis is a reminder that the deadline for your task \"%s\" is approaching in less than 12 hours.\n\n" +
+//                                    "**Task in Project:** %s\n" +
+//                                    "- **Task Name:** %s\n" +
+//                                    "- **Deadline:** %s\n\n" +
+//                                    "Please ensure that you complete the task before the deadline.\n\n" +
+//                                    "Best regards,\nNNKD",
+//                            user.getUsername(), task.getName(), project.getName(), task.getName(), formatter.format(deadline));
+//
+//                    MailSendRequest mailSendRequest = MailSendRequest
+//                            .builder()
+//                            .to(user.getEmail()).text(text).subject(subject).build();
+//
+//                    mailService.sendMail(mailSendRequest);
+//                    taskRequestService.updateSendMailDeadline(task.getId());
+//                }
+//            }
+//        }
+//    }
 
 
 
